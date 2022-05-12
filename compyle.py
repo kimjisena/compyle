825e9cb4f65e388b872096632f20ef0c74bd08de
jisena.btc <pjohmaths@gmail.com>
2022-05-12 18:18:59 +0300

    implemented watch functionality

--- compyle.py
+++ compyle.py
@@ -2,8 +2,18 @@
 
 import os, sys
 
-# compyle --watch src:dest
-fileStat = {}
+# compyle src:dest
+def watch():
+    fileStat = {}
+    inFile, outFile = sys.argv[1].split(':')
+    fileStat['lastMod'] = os.stat(inFile).st_mtime
 
-for arg in sys.argv:
-    print(arg)
+    print('Watching {} for changes...'.format(inFile))
+    while True:
+        if os.stat(inFile).st_mtime != fileStat['lastMod']:
+            print('{} changed...'.format(inFile))
+            fileStat['lastMod'] = os.stat(inFile).st_mtime
+        else:
+            continue
+
+if __name__ == '__main__': watch()
--- hello.c
+++ hello.c
@@ -2,4 +2,6 @@
 
 int main () {
     printf("hello world!");
+    printf("hello world again!");
+    printf("the return of hello world!");
 }
